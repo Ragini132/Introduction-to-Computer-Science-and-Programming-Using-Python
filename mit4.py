3a426034d212d3d2d433c2b095c658c4e271347d
Ragini132 <46533864+Ragini132@users.noreply.github.com>
2019-01-18 02:25:45 +0530

    Add files via upload

---
+++ mit4.py
@@ -0,0 +1,5 @@
+for i in range(12):
+    balance = balance - (balance * monthlyPaymentRate) + ((balance - (balance * monthlyPaymentRate)) * (annualInterestRate/12))
+print("Remaining balance:", round(balance, 2))
+
+
